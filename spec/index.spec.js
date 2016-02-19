cc3310e9ad577c6f2d78da54dc3feb4030239939
Simon MacDonald <simon.macdonald@gmail.com>
2016-02-18 20:36:06 -0500

    Remove tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -5,7 +5,6 @@
  */
 
 var cordova = require('./helper/cordova'),
-    Template = require('../www/template'),
     execSpy,
     execWin,
     options;
@@ -14,57 +13,9 @@ var cordova = require('./helper/cordova'),
  * Specification.
  */
 
-describe('cordova-plugin-template', function () {
+describe('phonegap-plugin-csdk-client-auth', function () {
     beforeEach(function () {
         execWin = jasmine.createSpy();
         execSpy = spyOn(cordova.required, 'cordova/exec').andCallFake(execWin);
-    });
-
-    describe('Template', function () {
-        it('should exist', function () {
-            expect(Template).toBeDefined();
-            expect(typeof Template === 'object').toBe(true);
-        });
-
-        it('should contain a echo function', function () {
-            expect(Template.echo).toBeDefined();
-            expect(typeof Template.echo === 'function').toBe(true);
-        });
-    });
-
-    describe('Template instance', function () {
-        describe('cordova.exec', function () {
-            it('should call cordova.exec on next process tick', function (done) {
-                Template.echo(function(data) {
-                    expect(data).toEqual('my message');
-                }, function() {}, 'my message', false);
-                setTimeout(function () {
-                    expect(execSpy).toHaveBeenCalledWith(
-                        jasmine.any(Function),
-                        jasmine.any(Function),
-                        'Echo',
-                        'echo',
-                        jasmine.any(Object)
-                    );
-                    done();
-                }, 100);
-            });
-
-            it('should call cordova.exec on next process tick async', function (done) {
-                Template.echo(function(data) {
-                    expect(data).toEqual('my message async');
-                }, function() {}, 'my message async', true);
-                setTimeout(function () {
-                    expect(execSpy).toHaveBeenCalledWith(
-                        jasmine.any(Function),
-                        jasmine.any(Function),
-                        'Echo',
-                        'echoAsync',
-                        jasmine.any(Object)
-                    );
-                    done();
-                }, 100);
-            });
-        });
     });
 });
